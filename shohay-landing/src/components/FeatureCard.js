25b56348d70fdb9370c82445bd36d5188083fe17
sohan nur <sohannur3@gmail.com>
2025-07-20 15:30:07 +0600

    inital demo landing page

---
+++ shohay-landing/src/components/FeatureCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FeatureCard({ icon, title, description }) {
+  return (
+    <div className="feature-card">
+      <div className="feature-icon">{icon}</div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default FeatureCard;
